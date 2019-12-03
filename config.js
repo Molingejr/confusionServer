916d32cb085a30d01b455593da75de27eca1d928
Molingejr <molingejr@gmail.com>
2019-12-03 09:37:48 +0100

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '744772212601131',
+        clientSecret: '7b7e0f1337f8f919131f2fc02baf219c'
+    }
 }
